2815b6e431b5b2dc1ff4580ebc59d56cb30ead3b
McLeanBH <mcleanbh@gmail.com>
2015-05-01 15:31:08 -0400

    Latency compensation-- 'demonstrate the order that posts appear using a slepp'

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,13 +1,13 @@
-Template.postSubmit.events({ 
+Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
 
     var post = {
-        url: $(e.target).find('[name=url]').val(), 
+        url: $(e.target).find('[name=url]').val(),
         title: $(e.target).find('[name=title]').val()
     };
 
-Meteor.call('postInsert', post, function(error, result) { 
+Meteor.call('postInsert', post, function(error, result) {
     // display the error to the user and abort
     if (error)
         return alert(error.reason);
@@ -15,8 +15,9 @@ Meteor.call('postInsert', post, function(error, result) {
     // show this result but route anyways
     if (result.postExists)
         alert('This link has already been posted');
-      
-    Router.go('postPage', {_id: result._id});
-    });
-  } 
+      });
+
+    Router.go('postPage'); // , {_id: result._id});
+
+  }
 });
